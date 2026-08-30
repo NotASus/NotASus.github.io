9604012557c8a41910af0e678c70cb89b3292c11
NotASus <mauri.valdes2002@gmail.com>
2026-08-30 09:07:25 -0400

    Agregar Resume en ingles, mostrar hora chilena actual (En teoria), configuraciones a los botones

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* Script de modificacion de botones*/
+
 function pageChange (button) {
     document.getElementById("Icontainer").src = button.dataset.src;
 
@@ -8,3 +10,21 @@ function pageChange (button) {
     button.disabled = true;
     button.classList.add("active");
 }
+
+/*Script para mostrar hora actual en Chile*/
+
+function UpdateChileTime() {
+    const option ={
+        timeZone: 'America/Santiago',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
+        timeZoneName: 'short'
+    };
+const ActualTime = new Intl.DateTimeFormat('es-CL', option).format(new Date());
+const el = document.getElementById('Chile-Time');
+if (el) el.textContent = formatted;
+}
+UpdateChileTime();
+setInterval(UpdateChileTime, 1000);
